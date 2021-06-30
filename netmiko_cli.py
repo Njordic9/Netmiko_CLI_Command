3315fe55f6ba2b16aa2fd4e102952f76124ee69c
Njordic9 <26882472+Njordic9@users.noreply.github.com>
2021-06-30 16:20:09 -0500

    Create netmiko_cli.py

---
+++ netmiko_cli.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Module docstring."""
+
+import sys
+import os
+import time
+import datetime
+import netmiko
+import keyring
+from netmiko import ConnectHandler
+from vchosts import all_devices
+from netmiko.ssh_exception import NetMikoTimeoutException, NetMikoAuthenticationException
+from paramiko.ssh_exception import SSHException
+from multiprocessing import Pool
+
+keyringPassword = keyring.get_password("label", "user")
+
+def start_commands(a_device, termuser):
+    try:
+        net_connect = ConnectHandler(**a_device)
+        hostname = net_connect.send_command("show hostname")
+        output = net_connect.send_command("vpn-sessiondb logoff name {} noconfirm").format(str(termuser))
+        print(hostname + " " + output)
+        net_connect.disconnect()
+    except (NetMikoTimeoutException, NetMikoAuthenticationException) as e:
+        print("Could not connect to " + a_device.get("ip") + ", Error: ", e)
+
+def apply_async_with_callback(termuser):
+    try:
+        with Pool(processes=10) as pool:
+            results = [pool.apply_async(start_commands, args = (a_device, termuser)) for a_device in all_devices]
+            pool.close()
+            pool.join()
+    except Exception as e:
+        print(e)
+
+# if __name__=='__main__':
+#     main()
